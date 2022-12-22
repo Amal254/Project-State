731963cc46a5a83adb50b97179856762c08f8174
Amal254 <amalbenammar8@gmail.com>
2022-12-22 23:06:36 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
+import React, { Component } from 'react'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export default class App extends Component {
+  state={
+    imageSrc : "/image/image1.png",
+    fullName:"Amal ben ammar",
+    bio:"Diplômé de L Institut Supérieur d Informatque et de Multmédia de Sfax (ISIMS)",
+    profession:"Designer graphique",
+    isShow:true,
 
-export default App;
+  }
+  handleShow=()=>this.setState({isShow:!this.state.isShow})
+  componentDidMount(){
+    console.log("componentDidMount")
+    setInterval(this.handleShow, 1000);
+  }
+  componentDidUpdate(){
+    console.log("componentDidUpdate")
+  }
+  componentWillUnmount(){
+    console.log("componentWillUnmount")
+  }
+  render() {
+    console.log("render")
+    return (
+      <div>
+      <button onClick={this.handleShow}>{this.state.isShow?"Hide":"Sow"}</button>
+      {
+        this.state.isShow? <div>
+        <img src='/image/image1.png' alt='image1'/>
+        <h1> {this.state.fullName}</h1>
+        <h2> {this.state.profession}</h2>
+        <h3> {this.state.bio}</h3>
+        </div>:null
+      }
+      </div>
+    )
+  }
+}
